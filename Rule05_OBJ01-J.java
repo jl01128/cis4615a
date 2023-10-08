0788511d7f064577492c5462f754fdf1079eaa46
jl01128 <jackie01128@gmail.com>
2023-10-08 18:32:55 -0400

    updated to compliant code

--- Rule05_OBJ01-J.java
+++ Rule05_OBJ01-J.java
@@ -1,6 +1,11 @@
 public class Widget {
-  public int total; // Number of elements
+  private int total; // Declared private
  
+  public int getTotal () {
+    return total;
+  }
+ 
+  // Definitions for add() and remove() remain the same
   void add() {
     if (total < Integer.MAX_VALUE) {     
       total++;
